925b4b71c0116ddfa9e0da89fdf5ac0b186862e7
Faraz <farazpourebrahimh@gmail.com>
2024-12-25 20:27:59 +0330

    [ADD] landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <h1>سلام، رفیق!</h1>;
+import { ReactElement } from "react";
+
+import SearchBox from "@/components/SearchBox/SearchBox";
+
+import styles from "./page.module.css";
+
+export default function Home(): ReactElement {
+  return (
+    <div className={styles.home}>
+      <h1>تورنیدو</h1>
+      <SearchBox />
+      <div className={styles.history}>
+        <div className={styles.title}>آخرین جستجوهای شما</div>
+        <ul>
+          <li>رامسر</li>
+          <li>مازندران</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
